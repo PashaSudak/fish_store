410d44646066daf4922ab5fa6ab249288152b4a4
Pasha <pavlosudak@gmail.com>
2022-04-24 14:07:54 +0300

    added model Product + connected to admin + added media path

--- store/fishstore/models.py
+++ store/fishstore/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Product (models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    availability = models.BooleanField(default = True)
+    img = models.ImageField(default = 'no_image.png', upload_to = 'product_image')
+
+    def __str__(self):
+        return f'{self.name}'
